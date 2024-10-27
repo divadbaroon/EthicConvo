ff08e8bd401f6ce253477627ebc0785f2b627b29
David <dbarron410@vt.edu>
2024-10-27 03:21:08 -0400

    auth sign up

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,21 @@
+import { SignUp } from '@clerk/nextjs'
+
+const SignUpPage = () => {
+  return (
+    <div className="flex justify-center items-start pt-10 min-h-screen mt-12">
+      <SignUp 
+        appearance={{
+          elements: {
+            card: "shadow-lg w-[500px] max-w-[95vw]",
+          },
+          layout: {
+            socialButtonsVariant: "iconButton",
+            socialButtonsPlacement: "bottom",
+          },
+        }}
+      />
+    </div>
+  )
+}
+
+export default SignUpPage
