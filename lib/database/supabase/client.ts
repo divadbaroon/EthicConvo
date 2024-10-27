22477874d16e001a16fb70074d6ef7e378cd5364
David <dbarron410@vt.edu>
2024-10-27 06:48:03 -0400

    normal client for supabase

---
+++ lib/database/supabase/client.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
+import { Database } from '@/types/supabase'
+
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL) throw new Error('Missing SUPABASE_URL')
+if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) throw new Error('Missing SUPABASE_ANON_KEY')
+
+export const supabaseAdmin = createClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
