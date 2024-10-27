dfbc6ae33f0c32ebce152e09860d6c8761b09281
David <dbarron410@vt.edu>
2024-10-27 03:41:13 -0400

    update navbar paths

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,7 @@
 
 // Navigation items
 export const NAV_ITEMS = [
-    { label: 'About Us', href: '/about' },
-    { label: 'For Researchers', href: '/researchers' },
+    { label: 'About Us', href: '/home/about' },
+    { label: 'For Researchers', href: '/home/researchers' },
     { label: 'Sessions', href: '/sessions' },
   ] as const;
