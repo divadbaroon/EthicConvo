9934c520d304fa3d300dd90e7d07542cfc571d2d
David <dbarron410@vt.edu>
2024-10-27 03:22:57 -0400

    added NAV_ITEMS

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+
+// Navigation items
+export const NAV_ITEMS = [
+    { label: 'About Us', href: '/about' },
+    { label: 'For Researchers', href: '/researchers' },
+    { label: 'Sessions', href: '/sessions' },
+  ] as const;
