fd5f2e2b0a59cafb6a1ae3cb8af197e13f0b1dcc
David <dbarron410@vt.edu>
2024-10-28 01:15:36 -0400

    changed name to supabaseClient

--- lib/database/supabase/client.ts
+++ lib/database/supabase/client.ts
@@ -4,7 +4,7 @@ import { Database } from '@/types/supabase'
 if (!process.env.NEXT_PUBLIC_SUPABASE_URL) throw new Error('Missing SUPABASE_URL')
 if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) throw new Error('Missing SUPABASE_ANON_KEY')
 
-export const supabaseAdmin = createClient<Database>(
+export const supabaseClient = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
